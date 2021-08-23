38d9940495c1d1a58beb2930729219f8970b4e62
Luke <sonicblister03@gmail.com>
2021-08-23 10:35:29 +1000

    Added basic configuration for mysql support

--- Volunteer_WebApp/Volunteer_WebApp/settings.py
+++ Volunteer_WebApp/Volunteer_WebApp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%c+e2@pis_-0ziusrj^07zmes53%yz3_5d%#z0y!p@srqk@qy3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deco3801-exbachina.uqcloud.net']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Volunteer_WebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
