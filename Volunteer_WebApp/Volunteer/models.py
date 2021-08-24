ee07110e87691d631f0ac9b6e9d40bbac670453a
Gupta, Prateek <i-prateek.gupta@monsterindia.com>
2021-08-24 15:37:26 +0530

    Created Events Model

--- Volunteer_WebApp/Volunteer/models.py
+++ Volunteer_WebApp/Volunteer/models.py
@@ -34,3 +34,14 @@ class Organization_Official(models.Model):
     
     def __str__(self):
 	    return self.name
+
+class Events(models.Model):
+    organization = models.OneToOneField(Organization_Official,null=False, blank= False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200,null = False)
+    photo = models.ImageField(default = "", null = True, blank = True)
+    description = models.CharField(max_length=10000,null = False, blank= False)
+    slots = models.PositiveIntegerField(default=10,validators=[MinValueValidator(1)], null=False)
+    roster = models.CharField(max_length=10000,null = False, blank= False)
+    contact = models.CharField(max_length=1000,null = False, blank= False)
+    location = models.CharField(max_length=10000,null = False, blank= False)
+    
