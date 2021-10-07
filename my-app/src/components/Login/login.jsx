19762f888c8b7185d6611ec6e9a5d9a4bcdc4b19
Luke <sonicblister03@gmail.com>
2021-10-07 14:52:09 +1000

    Updated login to display results without needing console, added dummy organisation to databse

--- my-app/src/components/Login/login.jsx
+++ my-app/src/components/Login/login.jsx
@@ -9,6 +9,7 @@ export class Login extends React.Component{
         organisations: [],
         username: '',
         password: '',
+        logResult: 0
     };
 
     constructor(props){
@@ -49,28 +50,31 @@ export class Login extends React.Component{
 
     handleLogin(e) {
         e.preventDefault();
+        this.state.logResult = 0;
         var user = this.state.username;
         var pass = this.state.password;
         var vol = this.state.volunteers;
         var org = this.state.organisations;
-        var isVol = false;
-        var isOrg = false;
         for (let i = 0; i < vol.count; i++) {
             //Address is temp until we add passwords
-            if (vol.results[i].name == user && vol.results[i].address == pass) {
-                isVol = true;
-            }
+            if (vol.results[i].name === user && vol.results[i].address === pass) {
+                console.log(">")
+                this.state.logResult = 1;            }
         }
         for (let i = 0; i < org.count; i++) {
-            if (org.results[i].name == user && org.results[i].address == pass) {
-                isOrg = true;
+            if (org.results[i].name === user && org.results[i].Organization === pass) {
+                this.state.logResult = 2;
             }
         }
-        if (isVol) {
+        console.log(this.state.logResult);
+        if (this.state.logResult === 1) {
             console.log("Valid Volunteer.");
-        } else if (isOrg) {
+        } else if (this.state.logResult === 2) {
             console.log("Valid Organisation.");
         } else {
+            this.setState({
+                logResult: -1
+            })
             console.log("Invalid User.");
         }
 
@@ -81,6 +85,9 @@ export class Login extends React.Component{
         <div className="all">
             <div className="login">
                 <div className = "h1">Login</div>
+                {this.state.logResult === 1 && <p className = 'valid'> Welcome, Volunteer. </p>}
+                {this.state.logResult === 2 && <p className = 'valid'> Welcome, Organisation Representative. </p>}
+                {this.state.logResult === -1 && <p className = 'invalid'> Error: Incorrect combination of username and password. Please try again. </p>}
                 <form className="form" onSubmit = {this.handleLogin}>
                     <input type="text" required="required" placeholder="UserName" name="u" value = {this.state.username} onChange = {this.usernameChange}></input>  
                     <input type="password" required="required" placeholder="Password" name="p" value = {this.state.password} onChange = {this.passwordChange}></input>  
