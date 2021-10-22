28b7030687a4758a782d28931ef20ced02718145
Luke <sonicblister03@gmail.com>
2021-10-22 14:34:05 +1000

    Fixed bug with backend denying post requests

--- Volunteer_WebApp/Volunteer_WebApp/settings.py
+++ Volunteer_WebApp/Volunteer_WebApp/settings.py
@@ -87,6 +87,10 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
