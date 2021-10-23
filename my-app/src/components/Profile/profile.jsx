2311c5b857bea7e29707f87ae4df315e40b4582d
Luke <sonicblister03@gmail.com>
2021-10-23 15:06:27 +1000

    Initial implementation of profile page

--- my-app/src/components/Profile/profile.jsx
+++ my-app/src/components/Profile/profile.jsx
@@ -1,13 +1,92 @@
 import React from "react";
-
+import axios from "axios";
 
 
 export class Profile extends React.Component{
+    
+    state = {
+        usertype: localStorage.getItem('userType'),
+        userID: localStorage.getItem('userID'),
+        data: [],
+        events: []
+    }
+
     constructor(props){
         super(props);
+
+
+    }
+
+    async componentDidMount() {
+        const usertype = this.state.usertype
+        const userID = this.state.userID
+        if (usertype == 'vol') {
+            axios.get('^api/volunteers').then(res => {
+                const data = res.data
+                for (let i = 0; i < data.count; i++) {
+                    if (data.results[i].id == userID) {
+                        this.setState({
+                            data: data.results[i]
+                        })
+                    }
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+            axios.get('^api/events').then(res => {
+                const data = res.data
+                var events = []
+                for (let i = 0; i < data.count; i++) {
+                    if (data.results[i].organization == userID) {
+                        events.push(data.results[i])
+                        this.setState({
+                            events: events
+                        })    
+                    }
+                }              
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+        } else if (usertype == 'org') {
+            axios.get('^api/organizations').then(res => {
+                const data = res.data
+                for (let i = 0; i < data.count; i++) {
+                    if (data.results[i].id == userID) {
+                        this.setState({
+                            data: data.results[i]
+                        })
+                    }
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+            axios.get('^api/events').then(res => {
+                const data = res.data
+                var events = []
+                for (let i = 0; i < data.count; i++) {
+                    if (data.results[i].organization == userID) {
+                        events.push(data.results[i])
+                        this.setState({
+                            events: events
+                        })    
+                    }
+                }
+                
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+        }
     }
 
     render(){
+        console.log(this.state.data)
+        //console.log(this.state.events)
+        const events = this.state.events
+        const data = this.state.data
         return(
         <div className="box">
             <div className="box1">
@@ -16,45 +95,15 @@ export class Profile extends React.Component{
                         <div className="d-flex flex-column align-items-center text-center">
                             <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"></img>
                             <div className="mt-3">
-                            <h4>John Doe</h4>
-                            <p>Full Stack Developer</p>
+                            <h4>{data.name}</h4>
+                            <p>{data.profession}</p>
                             <p>Bay Area, San Francisco, CA</p>
-                            <div className="bb">
-                                <button className="but">Follow</button>
-                            <button className="but">Message</button>
-                            </div>
                             </div>
                         </div>
                     </div>
                 </div>
             
-                <div className="user1"> 
-                    <ul className="ul1">
-                        <li>
-                        <h6 className="title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter mr-2 icon-inline text-info">
-                                <path 
-                                d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-                                />
-                                
-                                </svg>
-                                Twitter</h6>
-                                <span className="a1">@lin</span>
-                        </li>
-                        <hr />
-                        <li>
-                        <h6 className="title">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
-                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                        </svg>Instagram</h6>
-                        <span className="a1">lin</span>
-                        </li>
-                        <hr />
-                        <li>
-                                <h6 className="title"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>Facebook</h6>
-                                <span className="a1">lin</span>
-                            </li>
-                    </ul>
-                </div>
+
             </div>
             <div className="box2">
             <div className="user">
@@ -83,7 +132,7 @@ export class Profile extends React.Component{
                             <h5>Email</h5>
                         </div>
                         <div className="inf">
-                            jianqi.lin@uq.net.au
+                            {data.email}
                         </div>
                     </div>
                     <hr />
@@ -102,19 +151,26 @@ export class Profile extends React.Component{
             <div className="user">
                 <div className="card-body">
                     <div className="fav">
-                        <h4>Volunteer experience</h4>
+                        <h4>{this.state.usertype == 'org' && 'Organisation'}
+                        {this.state.usertype == 'vol' && 'Volunteer'} Experience:</h4>
                     </div>
                 </div>
             </div>
             <div className="user">
                 <div className="card-body">
                     <div className="fav">
-                        <h4>My Application</h4>
-                        <div className="event">
-                        <img src={require('../../images/tree.jpg')} alt='eventPic' />
-                            <h5>Volunteer activities for planting trees.</h5>
-                            <span className="inf">Location: sunnybank, Brisbane</span>
-                        </div>
+                        {this.state.usertype == 'org' && <h4>Events I'm Managing</h4>}
+                        {this.state.usertype == 'vol' && <h4>Events I'm Participating In</h4>}
+                        {events.map((event) => (
+                            <div id = "eventCard">
+                                <h5> <strong>Event:</strong> {event.title} </h5>
+                                <h5> <strong>Current Roster:</strong> {event.roster} </h5>
+                                <h5> <strong>Remaining Slots:</strong> {event.slots} </h5>
+                                <span className="loc">Location: {event.location}</span>
+                                <hr />
+                            </div>
+
+                        ))}
                     </div>
                 </div>
             </div>
@@ -123,3 +179,21 @@ export class Profile extends React.Component{
         )}
 
 }
+
+/*<ul>
+          {cards.map((event) => (
+            <li key = {event.id} style = {{
+              listStyleType: 'none'
+              }} >
+                
+              <EventCard 
+            additional={event.additional}
+            title={event.title}
+            description={event.description} 
+            slots={event.slots}
+            contact={event.contact}
+            location={event.location}
+            id={event.id}/>
+            </li>
+          ))}    
+        </ul>*/
